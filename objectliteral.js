9ff572fa3f95c4c71472a5dc6901839a4b25ffe7
PriyankaGanesh-L <mail.priyankalakshmanan@gmail.com>
2017-05-31 22:33:35 +0530

    Assignment 5.2
    Please run in chrome console

---
+++ objectliteral.js
@@ -0,0 +1,13 @@
+// JavaScript source code
+var vdob = new Date("2000-03-25").toDateString();
+var Student = {
+    name: 'priyanka',
+    age: 25,
+    dob: vdob,
+    pob :'Chennai'
+}
+console.log("Student Details");
+console.log("The name is " + Student.name);
+console.log("The age is " + Student.age);
+console.log("The dob is " + Student.dob);
+console.log("The place of birth is " + Student.pob);
